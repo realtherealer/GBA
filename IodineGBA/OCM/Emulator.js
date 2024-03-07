ab778e8af17038b9a821a992e5127b9c4e4c2e60
realtherealer <realtherealer@gmail.com>
2024-03-07 20:33:18 +0000

    Import OCM code

--- IodineGBA/OCM/Emulator.js
+++ IodineGBA/OCM/Emulator.js
@@ -11,15 +11,15 @@
 function GameBoyAdvanceEmulator() {
     console.log("AgbEMU GBAE func: Attempted")
     this.settings = {
-        "SKIPBoot":false,                   //Skip the BIOS boot screen.
+        "SKIPBoot":true,                   //Skip the BIOS boot screen.
         "audioVolume":1,                    //Starting audio volume.
         "audioBufferUnderrunLimit":8,       //Audio buffer minimum span amount over x interpreter iterations.
         "audioBufferDynamicLimit":2,        //Audio buffer dynamic minimum span amount over x interpreter iterations.
         "audioBufferSize":20,               //Audio buffer maximum span amount over x interpreter iterations.
-        "timerIntervalRate":16,             //How often the emulator core is called into (in milliseconds).
+        "timerIntervalRate":14,             //How often the emulator core is called into (in milliseconds).
         "emulatorSpeed":1,                  //Speed multiplier of the emulator.
-        "metricCollectionMinimum":30,       //How many cycles to collect before determining speed.
-        "dynamicSpeed":true                 //Whether to actively change the target speed for best user experience.
+        "metricCollectionMinimum":120,       //How many cycles to collect before determining speed.
+        "dynamicSpeed":false                 //Whether to actively change the target speed for best user experience.
     }
     this.audioFound = false;                  //Do we have audio output sink found yet?
     this.loaded = false;                      //Did we initialize IodineGBA?
